8f9e469a6048a770094535dca56689ff76c90679
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2023-10-20 21:47:09 +0530

    "first commit"

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "bb89e332216398477254567df9c184e1",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
